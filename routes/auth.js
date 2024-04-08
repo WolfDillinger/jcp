5d6e46ab03523f1af71b5b288d45e8a60b36dd5d
WolfDillinger <66114913+WolfDillinger@users.noreply.github.com>
2024-04-08 13:25:20 +0300

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const User = require("../models/user");
+const myAuth = require("../components/my_auth");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authReouter = express.Router();
+
+
+authReouter.post("/api/signup", async (req, res) => {
+
+    try {
+        const { fName, lName, phone, address, password, date } = req.body;
+
+        const findUser = await User.findOne({ phone });
+
+        if (findUser) {
+            return res.status(400).json({ meg: 'this phone number is already used', });
+        }
+
+        const hPassword = await bcrypt.hash(password, 8);
+
+        let user = new User({ fName, lName, address, phone, password: hPassword, date  });
+
+        const saveUser = await user.save();
+
+        const token = jwt.sign({ id: user._id }, "myKeyPass");
+
+        res.json({ token, ...saveUser._doc });
+        
+    } catch (err) {
+        res.status(500).json({ error: err.meg });
+    }
+
+});
+
+authReouter.post("/api/signin", async (req, res) => {
+    try {
+        const { phone, password } = req.body;
+
+        const user = await User.findOne({ phone });
+
+        if (!user) {
+            return res.status(400).json({ meg: 'this phone number not register', });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({ meg: 'Wrong pasword', });
+        }
+
+        const token = jwt.sign({ id: user._id }, "myKeyPass");
+
+        res.json({ token, ...user._doc });
+    } catch (err) {
+        res.status(500).json({ error: err.meg });
+    }
+});
+
+authReouter.post("/api/isValidToken", async (req, res) => {
+
+    try {
+        const token = req.header('jcp-auth');
+
+        if (!token) return res.json(false);
+
+        const verfifid = jwt.verify(token, "myKeyPass");
+
+        if (!verfifid) {
+            return res.json(false);
+        } else {
+            const user = await User.findByid(verfifid.id);
+            if (user) {
+                return res.json(true);
+            }
+            else {
+                return res.json(false);
+            }
+
+        }
+
+    } catch (err) {
+        res.status(500).json({ error: err.meg });
+    }
+});
+
+authReouter.get( "/",myAuth ,async(req,res)=>{
+    
+   const user = await User.findByid(req.userId);  
+     
+   res.json({...user._doc,token:req.token});  
+});
+
+module.exports = authReouter;
